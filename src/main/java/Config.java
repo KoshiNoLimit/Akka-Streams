4daec01d760b53ffb0f42b23fb27bc5f0a5dfa77
KoshiNoLimit <zmsh04@gmail.com>
2019-11-29 18:13:26 +0300

    Scripted auto-commit on change (2019-11-29 18:13:26) by gitwatch.sh

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,3 +1,6 @@
 public class Config {
-    
+    public static final String HOST = "localhost";
+    public static final Integer PORT = 8080;
+    public static final String ON_START = "start!";
+
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -14,7 +14,7 @@ import java.util.concurrent.CompletionStage;
 public class Server {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("start!");
+        System.out.println(Config.ON_START);
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
