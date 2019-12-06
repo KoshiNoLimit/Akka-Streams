834b9a56bff3aaadda18d7fc47e84ca88ec0eedc
KoshiNoLimit <zmsh04@gmail.com>
2019-12-07 02:44:41 +0300

    Scripted auto-commit on change (2019-12-07 02:44:41) by gitwatch.sh

--- src/main/java/ActorExplorer.java
+++ src/main/java/ActorExplorer.java
@@ -1,10 +1,17 @@
 import akka.actor.AbstractActor;
+import akka.http.javadsl.model.ws.TextMessage;
 import akka.japi.pf.ReceiveBuilder;
 
 public class ActorExplorer extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return ReceiveBuilder.create().
-                match
+        return ReceiveBuilder.create()
+                .match(FindMessage.class, msg -> {
+
+                })
+                .match(TestMessage.class, msg -> {
+
+                }).
+        build();
     }
 }
