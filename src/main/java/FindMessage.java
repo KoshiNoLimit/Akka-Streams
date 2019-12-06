9e85c8465a731beb519355f6cc43cfaa39ad3892
KoshiNoLimit <zmsh04@gmail.com>
2019-12-07 02:51:17 +0300

    Scripted auto-commit on change (2019-12-07 02:51:17) by gitwatch.sh

--- src/main/java/FindMessage.java
+++ src/main/java/FindMessage.java
@@ -1,2 +1,11 @@
 public class FindMessage {
+    private String url;
+
+    FindMessage(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
--- src/main/java/TestMessage.java
+++ src/main/java/TestMessage.java
@@ -1,2 +1,17 @@
 public class TestMessage {
+    private String url;
+    private Integer count;
+
+    TestMessage(String url, Integer count) {
+        this.url = url;
+        this.count = count;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
