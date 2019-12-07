a6bad565c723f857a4ab53cc64de75ffe8e839dc
KoshiNoLimit <zmsh04@gmail.com>
2019-12-07 04:34:56 +0300

    Scripted auto-commit on change (2019-12-07 04:34:56) by gitwatch.sh

--- src/main/java/ActorExplorer.java
+++ src/main/java/ActorExplorer.java
@@ -2,15 +2,19 @@ import akka.actor.AbstractActor;
 import akka.http.javadsl.model.ws.TextMessage;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ActorExplorer extends AbstractActor {
-    private Map<String, Long>
+    private Map<String, Long> store = new HashMap<>();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(FindMessage.class, msg -> {
+                    if(!store.containsKey(msg.getUrl())) {
+                        
+                    }
 
                 })
                 .match(TestMessage.class, msg -> {
