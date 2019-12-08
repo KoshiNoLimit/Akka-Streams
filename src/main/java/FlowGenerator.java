d43bef8971dae92b52b7f604b4f02e89f8a04b2f
KoshiNoLimit <zmsh04@gmail.com>
2019-12-09 02:27:20 +0300

    Scripted auto-commit on change (2019-12-09 02:27:20) by gitwatch.sh

--- src/main/java/FlowGenerator.java
+++ src/main/java/FlowGenerator.java
@@ -1,4 +1,76 @@
-package PACKAGE_NAME;
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.http.javadsl.model.*;
+import akka.pattern.Patterns;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+import akka.stream.Supervision;
+import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Keep;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import javafx.util.Pair;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.Response;
+
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import static config.Config.*;
+import static org.asynchttpclient.Dsl.asyncHttpClient;
 
 public class FlowGenerator {
+
+    static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorRef explorer, ActorMaterializer materializer) {
+
+        return Flow.of(HttpRequest.class)
+                .map(msg -> {
+                    Query q = msg.getUri().query();
+                    String url = q.get(ULR_PARAMETER).get();
+                    int count = Integer.parseInt(q.get(COUNT_PARAMETER).get());
+                    return new Pair<>(url, count);
+
+                }).mapAsync(MAX_STREAMS,  msg ->
+                        Patterns.ask(explorer, new FindMessage(msg.getKey()), TIMEOUT)
+                                .thenCompose(answer ->
+                                        answer.getClass() == TestMessage.class ?
+                                                CompletableFuture.completedFuture((TestMessage) answer)
+                                                :takeSource(msg, materializer))
+
+                ).map(answer -> {
+                    explorer.tell(answer, ActorRef.noSender());
+                    return HttpResponse
+                            .create()
+                            .withStatus(StatusCodes.OK)
+                            .withEntity(
+                                    HttpEntities.create(
+                                            answer.getUrl() + " " + answer.getCount()));
+                });
+    }
+
+    private static CompletionStage<TestMessage> takeSource (Pair<String, Integer> pair, Materializer materializer) {
+        return Source.from(Collections.singletonList(pair))
+                .toMat(testSink(), Keep.right())
+                .run(materializer)
+                .thenCompose(sum -> CompletableFuture.completedFuture(new TestMessage(pair.getKey(),sum/pair.getValue())));
+    }
+
+    private static Sink<Pair<String, Integer>, CompletionStage<Long>> testSink() {
+        return Flow.<Pair<String, Integer>>create()
+                .mapConcat(msg ->
+                        Collections.nCopies(msg.getValue(), msg.getKey()))
+                .mapAsync(MAX_STREAMS, url -> {
+                    long zeroTime = System.nanoTime();
+                    AsyncHttpClient client = asyncHttpClient();
+
+                    return client
+                            .prepareGet(url)
+                            .execute()
+                            .toCompletableFuture()
+                            .exceptionally(throwable -> (Response) Supervision.stop())
+                            .thenApply(response -> System.nanoTime() - zeroTime);
+                }).toMat(Sink.fold(0L, Long::sum), Keep.right());
+    }
+
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -5,28 +5,16 @@ import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.model.*;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
-import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
-import akka.stream.Materializer;
-import akka.stream.Supervision;
 import akka.stream.javadsl.Flow;
-import akka.stream.javadsl.Keep;
-import akka.stream.javadsl.Sink;
-import akka.stream.javadsl.Source;
-import javafx.util.Pair;
-import org.asynchttpclient.AsyncHttpClient;
-import org.asynchttpclient.Response;
-
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
 import static config.Config.*;
-import static org.asynchttpclient.Dsl.asyncHttpClient;
 
 public class Server  extends AllDirectives {
 
@@ -37,7 +25,7 @@ public class Server  extends AllDirectives {
 
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow(explorer, materializer);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = FlowGenerator.createFlow(explorer, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost(HOST, PORT),
@@ -48,56 +36,5 @@ public class Server  extends AllDirectives {
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
-    }
-
-    private static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorRef explorer, ActorMaterializer materializer) {
-
-        return Flow.of(HttpRequest.class)
-                .map(msg -> {
-                    Query q = msg.getUri().query();
-                    String url = q.get(ULR_PARAMETER).get();
-                    int count = Integer.parseInt(q.get(COUNT_PARAMETER).get());
-                    return new Pair<>(url, count);
-
-                }).mapAsync(MAX_STREAMS,  msg ->
-                        Patterns.ask(explorer, new FindMessage(msg.getKey()), TIMEOUT)
-                                .thenCompose(answer ->
-                                    answer.getClass() == TestMessage.class ?
-                                        CompletableFuture.completedFuture((TestMessage) answer)
-                                        :takeSource(msg, materializer))
-
-                ).map(answer -> {
-                    explorer.tell(answer, ActorRef.noSender());
-                    return HttpResponse
-                        .create()
-                        .withStatus(StatusCodes.OK)
-                        .withEntity(
-                            HttpEntities.create(
-                                answer.getUrl() + " " + answer.getCount()));
-                });
-    }
-
-    private static CompletionStage<TestMessage> takeSource (Pair<String, Integer> pair, Materializer materializer) {
-        return Source.from(Collections.singletonList(pair))
-                .toMat(testSink(), Keep.right())
-                .run(materializer)
-                .thenCompose(sum -> CompletableFuture.completedFuture(new TestMessage(pair.getKey(),sum/pair.getValue())));
-    }
-
-    private static Sink<Pair<String, Integer>, CompletionStage<Long>> testSink() {
-        return Flow.<Pair<String, Integer>>create()
-                .mapConcat(msg ->
-                        Collections.nCopies(msg.getValue(), msg.getKey()))
-                .mapAsync(MAX_STREAMS, url -> {
-                    long zeroTime = System.nanoTime();
-                    AsyncHttpClient client = asyncHttpClient();
-
-                    return client
-                            .prepareGet(url)
-                            .execute()
-                            .toCompletableFuture()
-                            .exceptionally(throwable -> (Response) Supervision.stop())
-                            .thenApply(response -> System.nanoTime() - zeroTime);
-                }).toMat(Sink.fold(0L, Long::sum), Keep.right());
     }
 }
